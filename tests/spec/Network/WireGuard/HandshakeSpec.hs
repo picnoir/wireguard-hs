4634b7b249cadd33053149adf8e258c3c9548b64
Félix Baylac-Jacqué <felix@alternativebit.fr>
2017-11-21 10:52:20 +0100

    Tested handshake init.

--- tests/spec/Network/WireGuard/HandshakeSpec.hs
+++ tests/spec/Network/WireGuard/HandshakeSpec.hs
@@ -1,55 +1,69 @@
 module Network.WireGuard.HandshakeSpec (spec) where
 
 import Test.Hspec                                  (Spec, describe,
-                                                    it, pending, around, shouldSatisfy,
+                                                    it, around, shouldSatisfy,
                                                     shouldNotBe, shouldBe)
 import Control.Monad.STM                           (atomically)
 import Control.Monad.Trans.Except                  (runExceptT)
 import Control.Exception                           (bracket)
-import Control.Concurrent.STM                      (putTMVar, writeTVar)
+import Control.Concurrent.STM                      (putTMVar, writeTVar, readTVarIO)
 import qualified Data.ByteArray             as BA  (convert)
 import qualified Data.ByteString.Char8      as BC  (pack)
 import qualified Data.ByteString            as BS  (empty)
 import           Data.Either                       (isRight)
-import           Data.Maybe                        (fromJust)
+import           Data.Maybe                        (fromJust, isJust)
 import           Data.Hex                          (unhex)
 import           Data.IP                           (AddrRange, IPv4, 
                                                     IPRange(..))
 import qualified Crypto.Noise.DH            as DH  (dhBytesToPub, dhBytesToPair)
+import Foreign.C.Types                             (CTime(..))
 import Network.Socket                              (SockAddr(..), tupleToHostAddress)
 import System.Posix.Time                           (epochTime)
 
+import Network.WireGuard.Internal.Constant         (handshakeRetryTime, handshakeStopTime)
 import Network.WireGuard.Internal.State            (Peer(..), Device(..), createPeer,
-                                                    createDevice)
-import Network.WireGuard.Internal.Data.Types       (PresharedKey)
+                                                    createDevice, initRekeyTimeout, initRekeyAttemptTime)
+import Network.WireGuard.Internal.Data.Types       (PresharedKey, Time)
 import Network.WireGuard.Internal.Data.Handshake   (HandshakeInitSeed(..))
 import Network.WireGuard.Internal.Stream.Handshake (handshakeInit)
 
 spec :: Spec
 spec = around withTestInitPeer $ describe "handshakeinit" $ do
-    it "should generate a correct udp packet" $ \(dev,peer) -> do
-        now <- epochTime
-        eKpHex <- unhex $ BC.pack "e84b5a6d2717c1003a13b431570353dbaca9146cf150c5f8575680feba52027a"
-        let eKp = fromJust $ DH.dhBytesToPair $ BA.convert eKpHex
-        let seed = HandshakeInitSeed eKp now 0
-        let remote = SockAddrInet 1337 $ tupleToHostAddress (192,168,1,1)
+    it "should generate a correct udp packet" $ \(dev,peer,seed) -> do
         eInitPacket <- atomically . runExceptT $ handshakeInit seed dev peer Nothing remote
         eInitPacket `shouldSatisfy` isRight
         let initPacket = fromRight eInitPacket
         initPacket `shouldNotBe` (BS.empty, undefined)
         snd initPacket `shouldBe` remote
-    it "should accordingly update the STM state" $ \_ -> pending 
+    it "should accordingly update the STM state" $ \(dev,peer,seed)  -> do
+        _ <- atomically . runExceptT $ handshakeInit seed dev peer Nothing remote
+        initState <- readTVarIO $ handshakeInitSt peer
+        let timeout = initRekeyTimeout <$> initState
+        timeout`shouldSatisfy` isJust
+    it "should correctly update the rekey timeout timer" $ \(dev,peer,seed) -> do
+        _ <- atomically . runExceptT $ handshakeInit seed dev peer Nothing remote
+        initState <- readTVarIO $ handshakeInitSt peer
+        let timeout = initRekeyTimeout <$> initState
+        fromJust timeout `shouldSatisfy` (> handshakeNowTS seed)
+        fromJust timeout `shouldBe` addTime (handshakeNowTS seed) handshakeRetryTime
+    it "should correctly update the rekey attempt time timer" $ \(dev,peer,seed) -> do
+        _ <- atomically . runExceptT $ handshakeInit seed dev peer Nothing remote
+        initState <- readTVarIO $ handshakeInitSt peer
+        let attempt = initRekeyAttemptTime <$> initState
+        fromJust attempt `shouldBe` addTime (handshakeNowTS seed) handshakeStopTime
+    where remote = SockAddrInet 1337 $ tupleToHostAddress (192,168,1,1)
 
 fromRight :: Either a b -> b
 fromRight (Right b) = b
 fromRight _ = error "Not right"
 
-withTestInitPeer :: ((Device,Peer) -> IO ()) -> IO ()
+withTestInitPeer :: ((Device,Peer,HandshakeInitSeed) -> IO ()) -> IO ()
 withTestInitPeer = bracket withRessources (\ _ -> return ())
     where withRessources = do
             dev  <- initDev
             peer <- initPeer 
-            return (dev,peer)
+            seed <- initSeed
+            return (dev,peer,seed)
                                 
 
 initDev :: IO Device
@@ -76,4 +90,12 @@ initPeer = do
         pubKey hex = fromJust . DH.dhBytesToPub $ BA.convert hex 
         ipRange = [IPv4Range (read "192.168.4.6/32" :: AddrRange IPv4)]
 
- 
+initSeed :: IO HandshakeInitSeed
+initSeed = do
+    now <- epochTime
+    eKpHex <- unhex $ BC.pack "e84b5a6d2717c1003a13b431570353dbaca9146cf150c5f8575680feba52027a"
+    let eKp = fromJust $ DH.dhBytesToPair $ BA.convert eKpHex
+    return $ HandshakeInitSeed eKp now 0
+
+addTime :: Time -> Int -> Time
+addTime (CTime now) secs = CTime (now + fromIntegral secs)
